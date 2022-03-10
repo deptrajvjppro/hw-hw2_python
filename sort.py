5a24f9b11a5d4e033348950c358e4ec6ce1ff2f6
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-10 00:20:38 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -11,9 +11,3 @@ def sort_list(list):
     return list
 
 
-unsorted_list = [34, 76, 90, 12, 32, 44, 55]
-sorted_list = sort_list(unsorted_list)
-print(sorted_list)
-list = [1,3,2,7]
-sorted_list = sort_list(list)
-print(sorted_list)
