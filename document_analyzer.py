41d99b9b2b64ac18ed91882fc99bad723ca5cec8
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-10 17:01:28 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,33 +1,26 @@
 import string
 
-file = open("document.txt", encoding="utf8")
-  
-d = {}
-  
-for line in file:
-    line = line.strip()
-    
-    line = line.lower()
-      
-    line = line.translate(line.maketrans("", "", string.punctuation))
- 
-    words = line.split(" ")
- 
+def document_analyzer(filename):
+    file = open(filename, encoding="utf8")
+    d = {}
+    for line in file:
+        line = line.strip()
+        
+        line = line.translate(line.maketrans("", "", string.punctuation))
+        words = line.split(" ")
     for word in words:
-       
         if word in d:
-          
             d[word] = d[word] + 1
         else:
-          
             d[word] = 1
+    sort_orders = sorted(d.items(), key=lambda x: (-x[1], x[0]))
+    first_five = list(sort_orders)[:5]
+    for i in first_five:
+        print(f'{i[0]}: {i[1]}')
+  
 
-    
-sort_orders = sorted(d.items(), key=lambda x: (-x[1], x[0]))
+document_analyzer("document.txt")
 
-first_five = list(sort_orders)[:5]
 
-for i in first_five:
-    print(f'{i[0]}: {i[1]}')
 
 
