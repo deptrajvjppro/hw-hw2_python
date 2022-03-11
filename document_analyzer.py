e9aef1dff114bb7350fa67efb385520378196435
deptrajvjppro <99304464+deptrajvjppro@users.noreply.github.com>
2022-03-10 17:09:04 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -2,17 +2,18 @@ import string
 
 def document_analyzer():
     file = open("document.txt", encoding="utf8")
-    d = {}
+   
     for line in file:
-        line = line.strip()
-        
         line = line.translate(line.maketrans("", "", string.punctuation))
         words = line.split(" ")
+    
+    d = {}
     for word in words:
         if word in d:
             d[word] = d[word] + 1
         else:
             d[word] = 1
+    
     sort_orders = sorted(d.items(), key=lambda x: (-x[1], x[0]))
     first_five = list(sort_orders)[:5]
     for i in first_five:
